cd1057652015e4eeb92d44660aee851ee9c79012
Fielding Johnston <fielding@justfielding.com>
2019-11-08 14:09:24 -0600

    Introduce working promoCards; still need to refactor and 'svelte it'
    # Explain why this change is being made
    
    # Provide links to any relevant tickets, articles or other resources

--- src/components/PromoCards.svelte
+++ src/components/PromoCards.svelte
@@ -1,58 +1,110 @@
 <script>
 	import { onMount } from 'svelte';
+	import { TOUCHES } from '../constants/device.ts';
 	import PromoCard from './PromoCard.svelte';
-	
-	const promoInfo = [
-		{
-			name: 'promo__friend_bonus',
-			title: '2x Friend Bonus',
-			body: 'Get a 5x bonus every time you and your friend watch the same Vidy',
-			screen: 'FriendBonus',
-		},
-		{
-			name: 'promo__lottery',
-			title: 'Win Lottery',
-			body: 'Theres a hidden jackpot under every 50 Vidys you hold. Good luck!',
-			screen: 'Lottery',
-		},
-		{
-			name: 'promo__onboarding',
-			title: 'Earn 50 VidyCoin',
-			body: 'Finish onboarding and earn 50 VidyCoin!',
-			screen: 'Onboarding1',
-		},
-		{
-			name: 'rewards',
-			title: 'Unlock Trophies',
-			body: 'Complete milestones and start earning VidyCoin!',
-			screen: 'Rewards',
-		},
-];
-	
-const total = promoInfo.length;
+	import promoInfo from '../data/promoCards.js';
 
-let cards;
-	
-onMount(() => {
-	const lastCardClone = cards.lastChild.cloneNode(true);
-	cards.appendChild(cards.firstChild.cloneNode(true));
-	cards.insertBefore(lastCardClone, cards.firstChild);
-	
-	const interval = setInterval(() => {
-			console.log('beep');
-		}, 1000);
+	let cards;
+	let current;
+	let intervalId;
 
-	return () => clearInterval(interval);
-});
+	const scrollToPromo = index => {
+		if ('scrollBehavior' in document.documentElement.style) {
+			requestAnimationFrame(() => cards.scrollTo({ left: cards.children[index].offsetLeft, top: 0, behavior: 'smooth' }));
+		} else {
+			dom.smoothHorizontalScroll(cards, cards.children[index].offsetLeft);
+		}
+	}
 
+	const prev = () => {
+		if (!document.hidden) {
+			if (current === 0) {
+				cards.scrollLeft = cards.children[promoInfo.length].offsetLeft;
+				current = promoInfo.length - 1;
+			} else {
+				current -= 1;
+			}
+			scrollToPromo(current);
+		}
+	};
 
+	const next = () => {
+		if (!document.hidden) {
+			if (current === promoInfo.length + 1) {
+				cards.scrollLeft = cards.children[1].offsetLeft;
+				current = 2;
+			} else {
+				current += 1;
+			}
+			scrollToPromo(current);
+		}
+	};
 
-	
+	onMount(() => {
+		const lastCardClone = cards.lastChild.cloneNode(true);
+		cards.appendChild(cards.firstChild.cloneNode(true));
+		cards.insertBefore(lastCardClone, cards.firstChild);
+
+		cards.scrollLeft = cards.children[1].offsetLeft;
+		current = 1
+
+		let intervalId = setInterval(next, 5000);
+
+		if (TOUCHES) {
+				// tmp = dom.create('button');
+				// tmp.className = styles['acc__profile__promos__prev'];
+				// tmp.onclick = e => {
+				// 	dom.prevent(e);
+				// 	intervalId = clearInterval(intervalId);
+				// 	prev();
+				// 	intervalId = setInterval(next, 5000);
+
+				// };
+				// dom.append(promos, tmp);
+
+				// tmp = dom.create('button');
+				// tmp.className = styles['acc__profile__promos__next'];
+				// tmp.onclick = e => {
+				// 	dom.prevent(e);
+				// 	intervalId = clearInterval(intervalId);
+				// 	next();
+				// 	intervalId = setInterval(next, 5000);
+				// };
+				// dom.append(promos, tmp);
+			} else {
+				let promoScrollTimeout;
+
+				cards.onscroll = () =>  {
+
+					// stop automatic scrolling
+					intervalId = clearInterval(intervalId);
+
+					// create looping illusion
+					// TODO: play around with threshold value to see if the scroll bouncing can be avoided/dampened
+					if (cards.scrollLeft <= cards.firstChild.offsetLeft) {
+						requestAnimationFrame(() => cards.scrollLeft = cards.scrollWidth - cards.children[1].offsetLeft - cards.clientWidth + cards.firstChild.offsetLeft);
+					}
+
+					if (cards.scrollLeft >= cards.scrollWidth - cards.clientWidth) {
+						requestAnimationFrame(() => cards.scrollLeft = cards.children[1].offsetLeft);
+					}
+
+					// update state after manual scrolling
+					clearTimeout(promoScrollTimeout);
+					promoScrollTimeout = setTimeout(() => {
+						intervalId = setInterval(next, 5000);
+						current = Math.floor(cards.scrollLeft / cards.clientWidth);
+					}, 66);
+				};
+			}
+
+		return () => clearInterval(intervalId);
+	});
 </script>
 
 <div class="promos" bind:this={cards}>
 	{#each promoInfo as {name, title, body, screen}, index}
-		<PromoCard {title} {body} {index} {total} />
+		<PromoCard {title} {body} {index} total={promoInfo.length} />
 	{/each}
 </div>
 
---
+++ src/data/promoCards.js
@@ -0,0 +1,28 @@
+const promoInfo = [
+	{
+		name: 'promo__friend_bonus',
+		title: '2x Friend Bonus',
+		body: 'Get a 5x bonus every time you and your friend watch the same Vidy',
+		screen: 'FriendBonus',
+	},
+	{
+		name: 'promo__lottery',
+		title: 'Win Lottery',
+		body: 'Theres a hidden jackpot under every 50 Vidys you hold. Good luck!',
+		screen: 'Lottery',
+	},
+	{
+		name: 'promo__onboarding',
+		title: 'Earn 50 VidyCoin',
+		body: 'Finish onboarding and earn 50 VidyCoin!',
+		screen: 'Onboarding1',
+	},
+	{
+		name: 'rewards',
+		title: 'Unlock Trophies',
+		body: 'Complete milestones and start earning VidyCoin!',
+		screen: 'Rewards',
+	},
+];
+
+export default promoInfo;
