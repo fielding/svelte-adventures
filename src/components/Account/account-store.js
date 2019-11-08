a9ce9d149d5105987c470e496e0a34c95ddf08e4
Fielding Johnston <fielding@justfielding.com>
2019-11-08 12:04:08 -0600

    Update account store
    # Explain why this change is being made
    
    # Provide links to any relevant tickets, articles or other resources

--- src/components/Account/account-store.js
+++ src/components/Account/account-store.js
@@ -1,9 +1,24 @@
-import { writable, derived} from 'svelte/store';
+import { get, writable, derived} from 'svelte/store';
 
 export const stackedScreen = writable(null);
 export const mainScreen = writable('Profile');
-export const mainTarget = writable(null);
+export const mainTarget = writable('Profile');
 
 export const mainClientWidth = writable(0);
 
 export const currentScreen = derived([mainScreen, stackedScreen], ([$mainScreen, $stackedScreen]) => $stackedScreen ? $stackedScreen.name : $mainScreen);
+
+// Initially I had wanted to include it as a method on a custom store, but if wasn't sure which store to do this with since
+// navigate updates both mainTarget and stackedScreen, and I didn't want to combine them in to a store that returned an object containing
+// both since they can be acted on independently of eachother.
+
+export const navigate = screen => {
+	let target = get(mainTarget);
+	if (typeof screen === 'function') {
+			mainScreen.update(mainScreen => target !== null ? target : mainScreen);
+			stackedScreen.update(stackedScreen => stackedScreen === screen ? null : screen);
+		} else if (typeof screen === 'string') {
+			mainTarget.set(screen);
+			stackedScreen.set(null);
+		}
+	};
