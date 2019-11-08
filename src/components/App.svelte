a76355b9bb176a3e80d53818c45428f6fbd2b493
Fielding Johnston <fielding@justfielding.com>
2019-11-08 05:58:32 -0600

    Add sane default global styles via App.svelte

--- src/components/App.svelte
+++ src/components/App.svelte
@@ -7,10 +7,13 @@
 <style>
 	:global(body) {
 		background-color: #0f0f0f;
+		font-family: 'Montserrat', -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Helvetica, Arial, sans-serif;
 	}
-	
+
 	:global(*) {
 		box-sizing: border-box;
+		-webkit-tap-highlight-color: rgba(0, 0, 0, 0);
+		-webkit-tap-highlight-color: transparent;
 	}
 </style>
 
