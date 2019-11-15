664ac9aac60ac2a5349b0128d1cf49eff8f54552
Fielding Johnston <fielding@justfielding.com>
2019-11-14 18:19:56 -0600

    Implement the rest of PromoCards

--- src/views/PromoCards/PromoCards.svelte
+++ src/views/PromoCards/PromoCards.svelte
@@ -1,24 +1,35 @@
 <script>
-	import { onMount } from 'svelte';
+	import { onDestroy, onMount } from 'svelte';
 	import { smoothHorizontalScroll } from '../../utils/dom.ts';
-	import { TOUCHES } from '../../constants/device.ts';
+	import { iOS, TOUCHES } from '../../constants/device.ts';
 	import PromoCard from './PromoCard.svelte';
-	import promoInfo from '../../data/promoCards.js';
+	import promoInfo from './data.js';
+	import { previouslyViewed } from './store.js';
 
 	let cards;
 	let current;
 	let intervalId;
+	let promoScrollTimeout;
 
-	const scrollToPromo = index => {
+	const scrollToPromo = (index, behavior = 'smooth') => {
 		if ('scrollBehavior' in document.documentElement.style) {
-			requestAnimationFrame(() => cards.scrollTo({ left: cards.children[index].offsetLeft, top: 0, behavior: 'smooth' }));
+			requestAnimationFrame(() => cards.scrollTo({ left: cards.children[index].offsetLeft, top: 0, behavior }));
 		} else {
-			smoothHorizontalScroll(cards, cards.children[index].offsetLeft);
+			if ( behavior === 'smooth') {
+				smoothHorizontalScroll(cards, cards.children[index].offsetLeft);
+			} else {
+				requestAnimationFrame(() =>{
+					if (iOS) cards.style['-webkit-overflow-scrolling'] = 'auto';
+						cards.scrollLeft = cards.children[index].offsetLeft;
+					if (iOS) cards.removeAttribute('style');
+				});
+			}
 		}
-	}
+	};
 
 	const prev = () => {
 		if (!document.hidden) {
+			intervalId = clearInterval(intervalId);
 			if (current === 0) {
 				cards.scrollLeft = cards.children[promoInfo.length].offsetLeft;
 				current = promoInfo.length - 1;
@@ -26,11 +37,13 @@
 				current -= 1;
 			}
 			scrollToPromo(current);
+			intervalId = setInterval(next, 5000);
 		}
 	};
 
 	const next = () => {
 		if (!document.hidden) {
+			intervalId = clearInterval(intervalId);
 			if (current === promoInfo.length + 1) {
 				cards.scrollLeft = cards.children[1].offsetLeft;
 				current = 2;
@@ -38,91 +51,73 @@
 				current += 1;
 			}
 			scrollToPromo(current);
+			intervalId = setInterval(next, 5000);
 		}
 	};
+
+
+	const handleScroll = async () => {
+		// stop automatic scrolling
+		intervalId = clearInterval(intervalId);
+		// clear previously used scrollTimeout
+		clearTimeout(promoScrollTimeout);
+		// create looping illusion
+		if (cards.scrollLeft <= cards.firstChild.offsetLeft) {
+			scrollToPromo(4, 'auto');
+		}
+
+		if (cards.scrollLeft >= cards.scrollWidth - cards.clientWidth) {
+			scrollToPromo(1, 'auto');
+		}
+
+		// update state after manual scrolling
+		promoScrollTimeout = setTimeout(() => {
+			if (!intervalId) intervalId = setInterval(next, 5000);
+			current = Math.floor(cards.scrollLeft / cards.clientWidth);
+		}, 66);
+	}
 
 	onMount(() => {
 		const lastCardClone = cards.lastChild.cloneNode(true);
 		cards.appendChild(cards.firstChild.cloneNode(true));
 		cards.insertBefore(lastCardClone, cards.firstChild);
 
-		cards.scrollLeft = cards.children[1].offsetLeft;
-		current = 1;
+		current = $previouslyViewed;
+		scrollToPromo(current, 'auto');
 
-		let intervalId = setInterval(next, 5000);
+		intervalId = setInterval(next, 5000);
+	});
 
-		if (!TOUCHES) {
-				// tmp = dom.create('button');
-				// tmp.className = styles['acc__profile__promos__prev'];
-				// tmp.onclick = e => {
-				// 	dom.prevent(e);
-				// 	intervalId = clearInterval(intervalId);
-				// 	prev();
-				// 	intervalId = setInterval(next, 5000);
-
-				// };
-				// dom.append(promos, tmp);
-
-				// tmp = dom.create('button');
-				// tmp.className = styles['acc__profile__promos__next'];
-				// tmp.onclick = e => {
-				// 	dom.prevent(e);
-				// 	intervalId = clearInterval(intervalId);
-				// 	next();
-				// 	intervalId = setInterval(next, 5000);
-				// };
-				// dom.append(promos, tmp);
-			} else {
-				let promoScrollTimeout;
-
-				cards.onscroll = () =>  {
-
-					// stop automatic scrolling
-					intervalId = clearInterval(intervalId);
-
-					// create looping illusion
-					// TODO: play around with threshold value to see if the scroll bouncing can be avoided/dampened
-					if (cards.scrollLeft <= cards.firstChild.offsetLeft) {
-						requestAnimationFrame(() => cards.scrollLeft = cards.scrollWidth - cards.children[1].offsetLeft - cards.clientWidth + cards.firstChild.offsetLeft);
-					}
-
-					if (cards.scrollLeft >= cards.scrollWidth - cards.clientWidth) {
-						requestAnimationFrame(() => cards.scrollLeft = cards.children[1].offsetLeft);
-					}
-
-					// update state after manual scrolling
-					clearTimeout(promoScrollTimeout);
-					promoScrollTimeout = setTimeout(() => {
-						intervalId = setInterval(next, 5000);
-						current = Math.floor(cards.scrollLeft / cards.clientWidth);
-					}, 66);
-				};
-			}
-
-		return () => clearInterval(intervalId);
+	onDestroy(() => {
+		clearInterval(intervalId);
+		clearTimeout(promoScrollTimeout);
+		$previouslyViewed = current;
 	});
 </script>
 
-<div class="promos" bind:this={cards}>
-	{#each promoInfo as {name, title, body, screen}, index}
-		<PromoCard {title} {body} {index} total={promoInfo.length} />
-	{/each}
+<div class="promoCards">
+	<div class="cards" bind:this={cards} on:scroll={handleScroll} >
+		{#each promoInfo as {name, title, body, screen}, index}
+			<PromoCard {title} {body} {index} total={promoInfo.length} />
+		{/each}
+	</div>
+	{#if !TOUCHES}
+		<button type="button" class="prev" on:click={prev}>
+			<svg viewBox="0 0 48 48" xmlns="http://www.w3.org/2000/svg">
+				<rect x="9.04602" y="24.0498" width="8.94109" height="33.8708" rx="4.47055" transform="rotate(-45 9.04602 24.0498)"/>
+				<rect x="15.3223" y="30.3181" width="8.94109" height="33.9353" rx="4.47055" transform="rotate(-135 15.3223 30.3181)"/>
+			</svg>
+		</button>
+		<button type="button" class="next" on:click={next}>
+			<svg viewBox="0 0 48 48" xmlns="http://www.w3.org/2000/svg">
+				<g transform="scale(-1, 1) translate(-48, 0)">
+					<rect x="9.04602" y="24.0498" width="8.94109" height="33.8708" rx="4.47055" transform="rotate(-45 9.04602 24.0498)"/>
+					<rect x="15.3223" y="30.3181" width="8.94109" height="33.9353" rx="4.47055" transform="rotate(-135 15.3223 30.3181)"/>
+				</g>
+			</svg>
+		</button>
+	{/if}
 </div>
 
-<style>
-	.promos {
-		max-width: 21em;
-		position: relative;
-		margin: .625em 1.1875em 0 1.1875em; /* 10px 19px 0 19px @ font-size 16 */
-		overflow-y: hidden;
-		overflow-x: scroll;
-		display: flex;
-		-webkit-overflow-scrolling: touch;
-		scroll-snap-type: mandatory; /* repeated below on purpose, this is to support legacy scroll snap api */
-		-ms-scroll-snap-type: x mandatory;
-		scroll-snap-type: x mandatory;
-		scrollbar-width: none;
-		-ms-overflow-style: none;
-	}
-</style>
+<style lang="scss" src="./style.scss"></style>
 
