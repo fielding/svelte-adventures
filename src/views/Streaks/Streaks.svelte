64e1d35bb5f5059b55f1fc445962204613d45193
Fielding Johnston <fielding@justfielding.com>
2019-11-15 05:45:58 -0600

    Seperate Streak's styles in to seperate file

--- src/views/Streaks/Streaks.svelte
+++ src/views/Streaks/Streaks.svelte
@@ -10,27 +10,4 @@ import response from './data.js';
 	</div>
 </AccountScreen>
 
-<style>
-	div {
-		height: 70%;
-		display: flex;
-		justify-content: center;
-		align-items: center;
-	}
-
-	img {
-		width: 75%;
-	}
-
-	.title {
-		font-weight: 500;
-    font-size: 1.125em; /* 18px with parent font-size 16px */
-    color: #ffffff;
-    text-transform: initial;
-    margin-top: 1em; /* same as this elements font-size */
-    margin-bottom: 1em;
-	}
-
-
-
-</style>
+<style lang="scss" src="styles.scss"></style>
