2a176fed813ce37960c6c35ef4cbbbaeef2a9bc9
Fielding Johnston <fielding@justfielding.com>
2019-11-16 02:40:32 -0600

    Seperate App.svelte in to two files and import styles/fonts.scss

--- src/views/App/App.svelte
+++ src/views/App/App.svelte
@@ -4,17 +4,7 @@
 
 <Account/>
 
-<style>
-	:global(body) {
-		background-color: #0f0f0f;
-		font-family: 'Montserrat', -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Helvetica, Arial, sans-serif;
-	}
-
-	:global(*) {
-		box-sizing: border-box;
-		-webkit-tap-highlight-color: rgba(0, 0, 0, 0);
-		-webkit-tap-highlight-color: transparent;
-	}
+<style lang="scss" src="styles.scss">
 </style>
 
 
