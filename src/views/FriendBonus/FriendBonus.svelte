bdf8921838e0920ad186c706d87dc400ed449003
Fielding Johnston <fielding@justfielding.com>
2019-11-16 02:42:40 -0600

    Update <FriendBonus> to same state as embed.js

--- src/views/FriendBonus/FriendBonus.svelte
+++ src/views/FriendBonus/FriendBonus.svelte
@@ -1,91 +1,95 @@
-<div class="acc__friendBonus">
-  <h2>2x Friend Bonus</h2>
-  <div class="shapes">
-    <h3>Rewards</h3>
-    <div class="acc__friendBonus__totalEarned row">
-      <span>Total Earned</span>
-      <span>82.375 VIDY</span>
-    </div>
-  </div>
-  <div class="card">
-    <h4>Friends</h4>
-    <div class="row">
-      <p>Invite friends to watch Vidys with you</p>
-      <button type="button">Invite Friends</button>
-    </div>
-    <div class="row">
-      <img
-        class="avatar"
-        alt="default avatar"
-        src="https://i.imgur.com/hZTluC5.png" />
-      <img
-        class="avatar"
-        alt="default avatar"
-        src="https://i.imgur.com/jyh02eu.png" />
-      <img
-        class="avatar"
-        alt="default avatar"
-        src="https://static.vidy.sh/users/fielding-83611-5a8f511000f2.jpg" />
-      <img
-        class="avatar"
-        alt="default avatar"
-        src="https://i.imgur.com/Qvby9sH.jpg" />
-      <svg
-        class="avatar"
-        xmlns="http://www.w3.org/2000/svg"
-        fill="none"
-        viewBox="0 0 42 42">
-        <path
-          fill="#fff"
-          fill-opacity="0.12"
-          fill-rule="evenodd"
-          d="M42 21c0 11.598-9.402 21-21 21C9.40202 42 0 32.598 0 21 0 9.40202
-          9.40202 0 21 0c11.598 0 21 9.40202 21 21zm-15.3701-4.8217c0
-          2.8598-2.3183 5.1782-5.1782 5.1782-2.8599
-          0-5.1783-2.3184-5.1783-5.1782 0-2.8599 2.3184-5.1783
-          5.1783-5.1783s5.1782 2.3184 5.1782 5.1783zm3.0467 13.9249c1.1305-.6178
-          1.2264-1.9741 1.2264-2.7958
-          0-2.0702-4.2316-3.7483-9.4515-3.7483-5.2199 0-9.4514 1.6781-9.4514
-          3.7483 0 .0188-.0001.0378-.0001.0572-.0006.8261-.0016 2.1349 1.1031
-          2.7386 1.7177.9385 5.2004.9525 8.3484.9525 3.148 0 6.5073-.014
-          8.2251-.9525z"
-          clip-rule="evenodd" />
-      </svg>
-      <svg
-        class="avatar"
-        xmlns="http://www.w3.org/2000/svg"
-        fill="none"
-        viewBox="0 0 42 42">
-        <path
-          fill="#fff"
-          fill-opacity="0.12"
-          fill-rule="evenodd"
-          d="M42 21c0 11.598-9.402 21-21 21C9.40202 42 0 32.598 0 21 0 9.40202
-          9.40202 0 21 0c11.598 0 21 9.40202 21 21zm-15.3701-4.8217c0
-          2.8598-2.3183 5.1782-5.1782 5.1782-2.8599
-          0-5.1783-2.3184-5.1783-5.1782 0-2.8599 2.3184-5.1783
-          5.1783-5.1783s5.1782 2.3184 5.1782 5.1783zm3.0467 13.9249c1.1305-.6178
-          1.2264-1.9741 1.2264-2.7958
-          0-2.0702-4.2316-3.7483-9.4515-3.7483-5.2199 0-9.4514 1.6781-9.4514
-          3.7483 0 .0188-.0001.0378-.0001.0572-.0006.8261-.0016 2.1349 1.1031
-          2.7386 1.7177.9385 5.2004.9525 8.3484.9525 3.148 0 6.5073-.014
-          8.2251-.9525z"
-          clip-rule="evenodd" />
-      </svg>
-    </div>
-  </div>
-  <div class="card">
-    <div class="row">
-      <span>Vidys Watched Together</span>
-      <span>15</span>
-    </div>
-  </div>
-  <div class="card">
-    <div class="row">
-      <span>Total 2x Bonus Earned</span>
-      <span>26.353 VIDY</span>
-    </div>
-  </div>
-</div>
+<script>
+	import AccountScreen from '~/views/AccountScreen/AccountScreen.svelte';
+</script>
 
-<style lang="scss" src="scss"></style>
+<AccountScreen>
+	<h2 name="title">2x Friend Bonus</h2>
+	<div class="shapes">
+		<h3>Rewards</h3>
+		<div class="totalEarned row">
+			<span>Total Earned</span>
+			<span>82.375 VIDY</span>
+		</div>
+	</div>
+	<div class="card">
+		<h4>Friends</h4>
+		<div class="row">
+			<p>Invite friends to watch Vidys with you</p>
+			<button type="button">Invite Friends</button>
+		</div>
+		<div class="row">
+			<img
+				class="avatar"
+				alt="default avatar"
+				src="https://i.imgur.com/hZTluC5.png" />
+			<img
+				class="avatar"
+				alt="default avatar"
+				src="https://i.imgur.com/jyh02eu.png" />
+			<img
+				class="avatar"
+				alt="default avatar"
+				src="https://static.vidy.sh/users/fielding-83611-5a8f511000f2.jpg" />
+			<img
+				class="avatar"
+				alt="default avatar"
+				src="https://i.imgur.com/Qvby9sH.jpg" />
+			<svg
+				class="avatar"
+				xmlns="http://www.w3.org/2000/svg"
+				fill="none"
+				viewBox="0 0 42 42">
+				<path
+					fill="#fff"
+					fill-opacity="0.12"
+					fill-rule="evenodd"
+					d="M42 21c0 11.598-9.402 21-21 21C9.40202 42 0 32.598 0 21 0 9.40202
+					9.40202 0 21 0c11.598 0 21 9.40202 21 21zm-15.3701-4.8217c0
+					2.8598-2.3183 5.1782-5.1782 5.1782-2.8599
+					0-5.1783-2.3184-5.1783-5.1782 0-2.8599 2.3184-5.1783
+					5.1783-5.1783s5.1782 2.3184 5.1782 5.1783zm3.0467 13.9249c1.1305-.6178
+					1.2264-1.9741 1.2264-2.7958
+					0-2.0702-4.2316-3.7483-9.4515-3.7483-5.2199 0-9.4514 1.6781-9.4514
+					3.7483 0 .0188-.0001.0378-.0001.0572-.0006.8261-.0016 2.1349 1.1031
+					2.7386 1.7177.9385 5.2004.9525 8.3484.9525 3.148 0 6.5073-.014
+					8.2251-.9525z"
+					clip-rule="evenodd" />
+			</svg>
+			<svg
+				class="avatar"
+				xmlns="http://www.w3.org/2000/svg"
+				fill="none"
+				viewBox="0 0 42 42">
+				<path
+					fill="#fff"
+					fill-opacity="0.12"
+					fill-rule="evenodd"
+					d="M42 21c0 11.598-9.402 21-21 21C9.40202 42 0 32.598 0 21 0 9.40202
+					9.40202 0 21 0c11.598 0 21 9.40202 21 21zm-15.3701-4.8217c0
+					2.8598-2.3183 5.1782-5.1782 5.1782-2.8599
+					0-5.1783-2.3184-5.1783-5.1782 0-2.8599 2.3184-5.1783
+					5.1783-5.1783s5.1782 2.3184 5.1782 5.1783zm3.0467 13.9249c1.1305-.6178
+					1.2264-1.9741 1.2264-2.7958
+					0-2.0702-4.2316-3.7483-9.4515-3.7483-5.2199 0-9.4514 1.6781-9.4514
+					3.7483 0 .0188-.0001.0378-.0001.0572-.0006.8261-.0016 2.1349 1.1031
+					2.7386 1.7177.9385 5.2004.9525 8.3484.9525 3.148 0 6.5073-.014
+					8.2251-.9525z"
+					clip-rule="evenodd" />
+			</svg>
+		</div>
+	</div>
+	<div class="card">
+		<div class="row">
+			<span>Vidys Watched Together</span>
+			<span>15</span>
+		</div>
+	</div>
+	<div class="card">
+		<div class="row">
+			<span>Total 2x Bonus Earned</span>
+			<span>26.353 VIDY</span>
+		</div>
+	</div>
+</AccountScreen>
+
+<style lang="scss" src="styles.scss"></style>
