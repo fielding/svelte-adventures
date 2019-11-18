375f5719c55a44d03ee32d16f695308d5dd8f576
Fielding Johnston <fielding@justfielding.com>
2019-11-18 13:31:32 -0600

    Add placeholder for <Onboarding>

---
+++ src/views/Onboarding/Onboarding.svelte
@@ -0,0 +1,13 @@
+<script>
+	import AccountScreen from '~/views/AccountScreen/AccountScreen.svelte';
+</script>
+
+
+<AccountScreen>
+	<h2 slot="title" class="title">Onboarding</h2>
+	<div>
+		Huzzah
+	</div>
+</AccountScreen>
+
+<style lang="scss" src="styles.scss"></style>
