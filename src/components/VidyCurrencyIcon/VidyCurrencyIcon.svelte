f189890d9468f6fb015f66d5fc9af1d34ee9f45c
Fielding Johnston <fielding@justfielding.com>
2019-11-21 18:00:54 -0600

    Clean up and fix typos

--- src/components/VidyCurrencyIcon/VidyCurrencyIcon.svelte
+++ src/components/VidyCurrencyIcon/VidyCurrencyIcon.svelte
@@ -5,7 +5,7 @@
 	export let fgColor = "#ffffff";
 </script>
 
-<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 48 48" role="img" aria-labledBy="title">
+<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 48 48" role="img" aria-labelledBy="title">
 	<title id="title">{title}</title>
 	<path fill={bgColor} d="M24 48c13.2548 0 24-10.7452 24-24S37.2548 0 24 0 0 10.7452 0 24s10.7452 24 24 24z"/>
 	<path fill={fgColor} d="M36.3897 15.1657l-8.623 20.7188h-7.5605l-8.5984-20.7188h7.5112l5.0157 12.637 5.2875-12.637h6.9675z"/>
